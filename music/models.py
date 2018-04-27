3a7c385317669d7373b2f97e6aab74df5eabde55
tlphillips <trex1964@gmail.com>
2018-04-27 13:30:45 -0400

    Note

--- music/models.py
+++ music/models.py
@@ -10,3 +10,5 @@ class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
+
+#this note
