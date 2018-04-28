51e44ce7d03ee67c90d3a02d8426663b7c5e069e
tlphillips <trex1964@gmail.com>
2018-04-28 10:55:06 -0400

    Note

--- music/views.py
+++ music/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>This is Music</h1>") 
+    return HttpResponse("<h1>This is Music</h1>")
+
+def detail(requst, album_id):
+    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+
