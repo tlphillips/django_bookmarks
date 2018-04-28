a3b60cfc860bfd6cfa6a5e02cb0c36c913e7252b
tlphillips <trex1964@gmail.com>
2018-04-28 11:44:28 -0400

    uodate

--- music/views.py
+++ music/views.py
@@ -4,10 +4,9 @@ from .models import Album
 def index(request):
     all_albums = Album.objects.all()
     html = ''
-
     for album in all_albums:
         url = '/music/' + str(album.id) + '/'
-        html += '<a href"' + url + '">' + album.album_title + '</a><br>'
+        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
     return HttpResponse(html)
 
 def detail(requst, album_id):
