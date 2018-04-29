8b35b2f481d2cf3643f24fd8a41b768be26917a8
tlphillips <trex1964@gmail.com>
2018-04-29 10:52:16 -0400

    uodate

--- django_bookmarks/settings.py
+++ django_bookmarks/settings.py
@@ -76,7 +76,21 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]﻿
 
 
 # Static files (CSS, JavaScript, Images)
