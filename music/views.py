8f2da0c04d89926afff0392c26013b776914166f
tlphillips <trex1964@gmail.com>
2018-04-29 11:07:54 -0400

    uodate

--- music/views.py
+++ music/views.py
@@ -6,6 +6,7 @@ import django
 
 def index(request):
     all_albums = Album.objects.all()
+    template = loader.get_template('/music/index.html')
     context = {
         'all_albums': all_albums,
     }
