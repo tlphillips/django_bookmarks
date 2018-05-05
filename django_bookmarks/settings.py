f251940fd76a86a5bcbdeed002ce1fc48b25a956
tlphillips <trex1964@gmail.com>
2018-05-05 07:15:38 -0400

    uodate

--- django_bookmarks/settings.py
+++ django_bookmarks/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = (
-    'music',
+    'music/templates/music',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
